d62e58369d75e3cbfce61d01ddff945f24dcb157
makise <10175101106@stu.ecnu.edu.cn>
2022-03-09 13:05:25 +0800

    test: rename file and test the correctness of constraints

--- verification/three_marabou_occlusion.py
+++ verification/three_marabou_occlusion.py
@@ -53,25 +53,7 @@ def verify_occlusion_with_fixed_size(image: np.array, label: int, occlusion_size
     c, h, w = image.shape
 
     # define the constraints on the entire image
-    constraints = []
-    for i in range(h):
-        for j in range(w):
-            eqs = []
-            for k in range(c):
-                eq = MarabouCore.Equation(MarabouCore.Equation.EQ)
-                eq.addAddend(1, inputs[k, i, j])
-                eq.setScalar(occlusion_color)
-                eqs.append(eq)
-            for ii in range(h):
-                for jj in range(w):
-                    if i == ii and j == jj:
-                        continue
-                    for kk in range(c):
-                        eq = MarabouCore.Equation(MarabouCore.Equation.EQ)
-                        eq.addAddend(1, inputs[kk, ii, jj])
-                        eq.setScalar(image[kk, ii, jj])
-                        eqs.append(eq)
-            constraints.append(eqs)
+    constraints = calculate_constrains(image, inputs)
 
     # the constraints should have size like [[eq1, eq2], [eq3, eq4], ...]
     # stand for (eq1 and eq1) or (eq2 and eq2) or ...
@@ -100,6 +82,43 @@ def verify_occlusion_with_fixed_size(image: np.array, label: int, occlusion_size
     print("vals length: ", len(vals))
 
     return vals, constraints_calculation_time, verify_time
+
+
+def calculate_constrains(image, inputs):
+    """
+    calculate the constraints for the entire image
+    :return: constraints
+    """
+    c, h, w = image.shape
+    constraints = []
+    test_constraints = []
+    for i in range(h):
+        for j in range(w):
+            eqs = []
+            test_eqs = []
+            for k in range(c):
+                eq = MarabouCore.Equation(MarabouCore.Equation.EQ)
+                eq.addAddend(1, inputs[k, i, j])
+                eq.setScalar(occlusion_color)
+                eqs.append(eq)
+                test_eq = f'x({k}, {i}, {j}) = {occlusion_color}'
+                test_eqs.append(test_eq)
+            for ii in range(h):
+                for jj in range(w):
+                    if i == ii and j == jj:
+                        continue
+                    for kk in range(c):
+                        eq = MarabouCore.Equation(MarabouCore.Equation.EQ)
+                        eq.addAddend(1, inputs[kk, ii, jj])
+                        eq.setScalar(image[kk, ii, jj])
+                        eqs.append(eq)
+                        test_eq = f'x({kk}, {ii}, {jj}) = {image[kk, ii, jj]}'
+                        test_eqs.append(test_eq)
+            constraints.append(eqs)
+            test_constraints.append(test_eqs)
+    print("test_constraints length: ", len(test_constraints))
+    print("test_constraints[0] length: ", len(test_constraints[0]))
+    return constraints
 
 
 if __name__ == '__main__':
