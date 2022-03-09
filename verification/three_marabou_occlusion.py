d92779666ede6a0d71ab293f52876fe54118b620
makise <10175101106@stu.ecnu.edu.cn>
2022-03-09 21:33:36 +0800

    feat: version-0.2 for thought three

--- verification/three_marabou_occlusion.py
+++ verification/three_marabou_occlusion.py
@@ -53,11 +53,73 @@ def verify_occlusion_with_fixed_size(image: np.array, label: int, occlusion_size
     c, h, w = image.shape
 
     # define the constraints on the entire image
-    constraints = calculate_constrains(image, inputs)
+    # constraints = calculate_constrains(image, inputs)
+    x = network.getNewVariable()
+    y = network.getNewVariable()
+    network.setLowerBound(x, 0)
+    network.setUpperBound(x, w - 1)
+    network.setLowerBound(y, 0)
+    network.setUpperBound(y, h - 1)
 
-    # the constraints should have size like [[eq1, eq2], [eq3, eq4], ...]
-    # stand for (eq1 and eq1) or (eq2 and eq2) or ...
-    network.addDisjunctionConstraint(constraints)
+    # iterate over the entire image
+    for i in range(h):
+        for j in range(w):
+            # occlusion point cover (i, j)
+            # the constraints should have size like [[eq1, eq2], [eq3, eq4], ...]
+            # stand for (eq1 and eq1) or (eq2 and eq2) or ...
+            # network.addDisjunctionConstraint(constraints)
+            constraints = []
+            eqs = []
+            eq1 = MarabouCore.Equation(MarabouCore.Equation.EQ)
+            eq1.addAddend(1, x)
+            eq1.addScalar(j)
+            eqs.append(eq1)
+            eq2 = MarabouCore.Equation(MarabouCore.Equation.EQ)
+            eq2.addAddend(1, y)
+            eq2.addScalar(i)
+            eqs.append(eq2)
+            for k in range(c):
+                eq3 = MarabouCore.Equation(MarabouCore.Equation.EQ)
+                eq3.addAddend(1, inputs[k][i][j])
+                eq3.addScalar(occlusion_color)
+                eqs.append(eq3)
+            constraints.append(eqs)
+            # otherwise
+            # since don't know how to write unequal constraints
+            # change two unequal constraints into four greater equal and less equal constraints
+            # and also don't know if there exists greater and less relation
+            eqs = []
+            for k in range(c):
+                eq8 = MarabouCore.Equation(MarabouCore.Equation.EQ)
+                eq8.addAddend(1, inputs[k][i][j])
+                eq8.addScalar(image[k][i][j])
+                eqs.append(eq8)
+            eqs_temp = eqs.copy()
+            eq4 = MarabouCore.Equation(MarabouCore.Equation.GE)
+            eq4.addAddend(1, x)
+            eq4.addScalar(j)
+            eqs.append(eq4)
+            constraints.append(eqs)
+            eqs = eqs_temp.copy()
+            eq5 = MarabouCore.Equation(MarabouCore.Equation.LE)
+            eq5.addAddend(1, x)
+            eq5.addScalar(j)
+            eqs.append(eq5)
+            constraints.append(eqs)
+            eqs = eqs_temp.copy()
+            eq6 = MarabouCore.Equation(MarabouCore.Equation.GE)
+            eq6.addAddend(1, y)
+            eq6.addScalar(i)
+            eqs.append(eq6)
+            constraints.append(eqs)
+            eqs = eqs_temp.copy()
+            eq7 = MarabouCore.Equation(MarabouCore.Equation.LE)
+            eq7.addAddend(1, y)
+            eq7.addScalar(i)
+            eqs.append(eq7)
+            constraints.append(eqs)
+            # add constraints to network
+            network.addDisjunctionConstraint(constraints)
 
     # add additional bounds for the inputs
     for i in range(h):
@@ -65,16 +127,16 @@ def verify_occlusion_with_fixed_size(image: np.array, label: int, occlusion_size
             for k in range(c):
                 network.setLowerBound(inputs[k, i, j], 0)
                 network.setUpperBound(inputs[k, i, j], 1)
-    constraints_calculation_end_time = time.monotonic()
-    constraints_calculation_time = constraints_calculation_end_time - constraints_calculation_start_time
 
     # add bounds to outputs
     for i in range(n_outputs):
         if i != label:
             network.addInequality([outputs_flattened[i], outputs_flattened[label]], [1, -1], 0)
+    constraints_calculation_end_time = time.monotonic()
+    constraints_calculation_time = constraints_calculation_end_time - constraints_calculation_start_time
 
     verify_start_time = time.monotonic()
-    vals, stats = network.solve()
+    vals, stats = network.solve(verbose=True)
     verify_end_time = time.monotonic()
     verify_time = verify_end_time - verify_start_time
 
