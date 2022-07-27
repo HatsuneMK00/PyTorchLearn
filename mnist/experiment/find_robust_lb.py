92c9f0244060242f70e9889e2df9b4c5e13a6150
makise <10175101106@stu.ecnu.edu.cn>
2022-07-27 17:07:15 +0800

    experiment: find lower bound of robustness

---
+++ mnist/experiment/find_robust_lb.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+# created by makise, 2022/7/26
+import concurrent.futures
+import time
+
+import pebble
+
+
+def determine_robustness(size, true_label, model, task):
+    colors = [0, 0.2, 0.4, 0.6, 0.8, 1]
+    for c in range(5):
+        position_range = (1 ,28 - size + 1)
+        step = (position_range[1] - position_range[0] + 1) // 4
+        position = [1, step, 2 * step, 3 * step, position_range[1]]
+        robust = True
+        for label in range(10):
+            start_time = time.monotonic()
+            if not robust:
+                break
+            if label == true_label:
+                continue
+            with pebble.ProcessPool(1) as pool:
+                color = (colors[c], colors[c + 1])
+                for i in range(4):
+                    a = (position[i], position[i + 1])
+                    b = (position[i], position[i + 1])
+                    size_a = (size, size)
+                    size_b = (size, size)
+                    future = pool.schedule(task, (model, label, a, b, size_a, size_b, color))
+                    try:
+                        print('label {}, a&b {}, size_a&b {}, color {}'.format(label, a, size_a, color), flush=True)
+                        result = future.result(timeout=60)
+                    except concurrent.futures.TimeoutError:
+                        future.cancel()
+                        print("timeout", flush=True)
+                        result = 'unsat' # To keep format with return value of task
+                    if result == 'sat':
+                        robust = False
+                        break
+            print("label {} end in {}".format(label, time.monotonic() - start_time))
+
+def find_robust_lower_bound(l, u, image_size, true_label, model, task):
+    lower = l
+    _, image_height, image_width = image_size
+    upper = u
+    upper_last_sat = upper
+    iter_count = 0
+    while upper - lower >= 1:
+        iter_count += 1
+        position_range = (1, image_height - lower + 1)
+        step = (position_range[1] - position_range[0] + 1) // 4
+        position = [1, step, 2 * step, 3 * step, position_range[1]]
+        robust = True
+        for label in range(10):
+            start_time = time.monotonic()
+            if not robust:
+                break
+            if label == true_label:
+                continue
+            with pebble.ProcessPool(1) as pool:
+                for i in range(4):
+                    a = (position[i], position[i + 1])
+                    b = (position[i], position[i + 1])
+                    size_a = (lower, upper)
+                    size_b = (lower, upper)
+                    future = pool.schedule(task, (model, label, a, b, size_a, size_b, (0, 0)))
+                    try:
+                        print('iteration {}: label {}, a&b {}, size_a&b {}'.format(iter_count, label, a, size_a), flush=True)
+                        result = future.result(timeout=60)
+                    except concurrent.futures.TimeoutError:
+                        future.cancel()
+                        print("iteration {} timeout".format(iter_count), flush=True)
+                        result = 'unsat' # To keep format with return value of task
+                    if result == 'sat':
+                        robust = False
+                        break
+            print("label {} end in {}".format(label, time.monotonic() - start_time))
+        if not robust:
+            print("size {} is not robust".format((lower, upper)), flush=True)
+            upper_last_sat = upper
+            upper = (upper + lower) // 2
+        else:
+            print("size {} is robust".format((lower, upper)), flush=True)
+            lower = upper
+            upper = (upper_last_sat + lower) // 2
+    return upper
+
+
---
+++ mnist/experiment/v3_on_fnn.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+# created by makise, 2022/7/14
+import json
+import time
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torchvision import datasets
+from torchvision import transforms
+import torch.utils.data as data
+from maraboupy import Marabou, MarabouCore
+
+from mnist.fnn_model_1 import FNNModel1
+from occlusion_layer.occlusion_layer_v3 import OcclusionLayer
+from find_robust_lb import find_robust_lower_bound
+import signal
+
+result = []
+
+def save_extended_model_onnx(image, model):
+    occlusion_layer = OcclusionLayer(image=image, first_layer=list(model.children())[0])
+    extended_model = nn.Sequential(
+        occlusion_layer,
+        *list(model.children())[1:]
+    )
+    extended_model = extended_model.to(torch.device('cpu'))
+    dummy_input = torch.tensor([1.0, 1.0, 1.0, 1.0, 0.0])
+    onnx_model_filename = 'tmp/v3/' + 'fnn_model_mnist_1_extended_shrink.onnx'
+    torch.onnx.export(extended_model, dummy_input, onnx_model_filename)
+    return onnx_model_filename
+
+
+def verify_with_marabou(model_filepath, label, a, b, size_a, size_b, color):
+    # signal.signal(signal.SIGALRM, handle_timeout)
+    network = Marabou.read_onnx(model_filepath)
+    inputs = network.inputVars[0]
+    outputs = network.outputVars
+    n_outputs = outputs.flatten().shape[0]
+    a_lower, a_upper = a
+    b_lower, b_upper = b
+    size_a_lower, size_a_upper = size_a
+    size_b_lower, size_b_upper = size_b
+    color_lower, color_upper = color
+    network.setLowerBound(inputs[0], a_lower)
+    network.setUpperBound(inputs[0], a_upper)
+    network.setLowerBound(inputs[1], size_a_lower)
+    network.setUpperBound(inputs[1], size_a_upper)
+    network.setLowerBound(inputs[2], b_lower)
+    network.setUpperBound(inputs[2], b_upper)
+    network.setLowerBound(inputs[3], size_b_lower)
+    network.setUpperBound(inputs[3], size_b_upper)
+    network.setLowerBound(inputs[4], color_lower)
+    network.setUpperBound(inputs[4], color_upper)
+
+    for i in range(n_outputs):
+        if i != label:
+            network.addInequality([outputs[i], outputs[label]], [1, -1], -1e-6)
+
+    options = Marabou.createOptions(solveWithMILP=False, verbosity=0)
+    # signal.alarm(60)
+    vals = network.solve(verbose=True, options=options)
+    # signal.alarm(0)
+
+    print("verification end for label: ", label, flush=True)
+    # print("result is ", vals[0])
+    return vals[0]
+
+def handle_timeout():
+    raise TimeoutError('verify over 1 min')
+
+
+
+if __name__ == '__main__':
+    test_loader = data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+        batch_size=1, shuffle=False)
+    iter_on_loader = iter(test_loader)
+    model = FNNModel1()
+    model.load_state_dict(torch.load('../../model/fnn_model_mnist_1.pth', map_location=torch.device('cpu')))
+    for i in range(20):
+        print("=" * 20)
+        print("image {}:".format(i))
+        instrument = {}
+        is_robust = True
+
+        total_time_start = time.monotonic()
+        image, label = iter_on_loader.next()
+
+        if i < 10 or i == 12 or i == 14 or i == 18:
+            continue
+
+        image = image.reshape(1, 28, 28)
+        label = label.item()
+        save_model_start = time.monotonic()
+        model_filepath = save_extended_model_onnx(image, model)
+        save_model_duration = time.monotonic() - save_model_start
+        instrument['save_model_duration'] = save_model_duration
+
+        verify_start = time.monotonic()
+        lower_bound = find_robust_lower_bound(5, 10, (1, 28, 28), label, model_filepath, verify_with_marabou)
+        verify_duration = time.monotonic() - verify_start
+        instrument['verify_duration'] = verify_duration
+        instrument['lower_bound'] = lower_bound
+        print(instrument)
+        result.append(instrument)
+
+    with open('result.json', 'w') as f:
+        json.dump(result, f)
+        f.write('\n')
+        f.flush()
+
+
+
+
